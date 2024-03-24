da3233553c61f656fbe69ad5f864fdfb926699d9
Sergio Laptop <escalantesergio821@gmail.com>
2024-03-24 18:52:20 -0400

    Modificado y implementado el asistente

--- lib/Controllers/getChatResponse.dart
+++ lib/Controllers/getChatResponse.dart
@@ -16,11 +16,21 @@ Future<void> getChatResponse(ChatMessage message, Function() updateStateCallback
     }
   }).toList();
 
-  final request = ChatCompleteText(
-    model: GptTurbo0301ChatModel(),
-    messages: messagesHistory.map((m) => m.toJson()).toList(),
-    maxToken: 100,
-  );
+//Aqui se define el modelo de chat que se va a utilizar y el objetivo del chat.
+//! No se pudo implementar el asistente debido a que tendriamos que refactorizar todo el proyecto, pero esta solucion es la mas parecida a un asistente.
+ final request = ChatCompleteText(
+  model: GptTurbo0301ChatModel(),
+  messages: [
+    {
+      "role": "system",
+      "content": "Tu principal objetivo es servir como el asistente óptimo para ayudar a los usuarios a manejar el estrés. Tus respuestas deben ser claras, cortas y concisas."
+    },
+    ...messagesHistory.map((m) => m.toJson()).toList()
+  ],
+  maxToken: 100,
+  temperature: 0.7,
+);
+
 
   final response = await OpenAIClient.openAI.onChatCompletion(request: request);
   if (response != null && response.choices.isNotEmpty) {
