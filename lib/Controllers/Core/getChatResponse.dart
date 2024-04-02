4195d7bcea8ffbb1424023592a4fa69b0df5ac54
Sergio Laptop <escalantesergio821@gmail.com>
2024-04-02 10:20:45 -0400

    Arreglado Prompt para que de respuestas mas cortas

--- lib/Controllers/Core/getChatResponse.dart
+++ lib/Controllers/Core/getChatResponse.dart
@@ -24,12 +24,12 @@ Future<void> getChatResponse(
       {
         "role": "system",
         "content":
-            "Como asistente definitivo para el manejo del estrés que actua como Sigmund Freud, su objetivo principal es brindar orientación concisa y clara a los usuarios que buscan aliviar el estrés. Adapte sus respuestas al nivel de estrés del usuario ajustando el tono y los detalles en consecuencia. Limite sus respuestas a un número de caracteres específico o 100 tokens y concluya cada respuesta con un porcentaje de estrés total."
+            "Quiero como actues como el psicolog Sigmund Freud, como asistente definitivo para el manejo del estrés, su objetivo principal es brindar orientación concisa y clara a los usuarios que buscan aliviar el estrés. Adapte sus respuestas al nivel de estrés del usuario ajustando el tono y los detalles en consecuencia. Limite sus respuestas a un número de caracteres específico de unas 10 palabras como maximo y concluya cada respuesta con un porcentaje de estrés total que este como lo siguiente (Nivel de estress: 'estress level')."
       },
       ...messagesHistory.map((m) => m.toJson()).toList()
     ],
     maxToken: 100,
-    temperature: 0.7,
+    temperature: 0.2,
   );
   
   final response = await OpenAIClient.openAI.onChatCompletion(request: request);
