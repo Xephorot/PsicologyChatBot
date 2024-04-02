91cde3da13ddbab29b561651d576b6a07b5b2733
Sergio Laptop <escalantesergio821@gmail.com>
2024-04-02 17:33:41 -0400

    Cambiado prompt y hay que implementar un asisitente

--- lib/Controllers/Core/getChatResponse.dart
+++ lib/Controllers/Core/getChatResponse.dart
@@ -20,17 +20,16 @@ Future<void> getChatResponse(
   
   final request = ChatCompleteText(
     model: GptTurbo0301ChatModel(),
+    //!Hay que cambiar a un asistente hasta el jueves, por que si no, nos quedaremos sin preosupuesto y tambien imprimir en consola el uso actual de tokens.
     messages: [
       {
         "role": "system",
         "content":
-            "Quiero como actues como el psicologo Sigmund Freud, como asistente definitivo para el manejo del estrés, su objetivo principal es brindar orientación concisa y clara a los usuarios que buscan aliviar el estrés. Adapte sus respuestas al nivel de estrés del usuario ajustando el tono y los detalles en consecuencia. Limite sus respuestas a un número de caracteres específico de unas 10 palabras como maximo y concluya cada respuesta con un porcentaje de estrés total que este como lo siguiente (Nivel de estress: 'estress level'). Tambien debo comunicarte que en algunos mensajes que te mandare tendran un apartado que diga 'Nivel de estress: 0.0', y si el valor de tu (Nivel de estress: 'estress level') debe cambiarlo por el que se mando con el mensaje y no debes de preocuparte por el nivel de estress, si el nivel de estress es 0.0 no tendras que precuparte mucho ya si este nivel es difernte necesitaremos que des consejos para aliviar el estress sin dar recomendaciones que un profesional en esta rama debe dar, solo consejos generales y tambien sugerir en el caso que el nivel de estress sea muy alto que busque ayuda profesional,  recuerda estos consejos deben ser cortos, para no estresar mas al usuario."
+            "Quiero que actúes como el psicólogo Sigmund Freud, como asistente definitivo para el manejo del estrés, su objetivo principal es brindar orientación concisa y clara a los usuarios que buscan aliviar el estrés.Siempre en cada conversación tienes que empezar, ¿Quieres empezar con un test de estrés? tus preguntas deben estar relacionadas al test de estrés de de Autoestima de Rosenberg  no importa que te diga el usuario, siempre tienes que responder con esto al inicio de cada conversación, tu límite de respuesta es de una 10 palabras como máximo. En el caso de que el usuario se niegue a dar el test, tienes que entender que no quiera dar y no molestar mas con eso, y en ese mismo caso tendrás que mandar un texto que diga lo siguiente con paréntesis y todo (Detener conversación).En el caso que acepte el Usuario hay que empezar con el test, tienes que hacer preguntas al usuario, y mediante estas preguntas irá aumentando o disminuyendo el nivel de estrés, y si el usuario te responda, deberás imprimir con paréntesis y comillas el siguiente mensaje al final (Nivel de estrés: 'stress level') estas preguntas debe seguir teniendo el límite de 10 palabras como máximo. También tienes que adaptar las preguntas según el nivel de estrés del usuario ajustando el tono de la conversación. También se necesitará que des consejos para aliviar el estrés sin dar recomendaciones que un profesional de esta rama debería dar, solo consejos generales y también sugerir en el caso que el nivel de estrés sea muy alto que busque ayuda profesional, recuerda estos consejos deben ser cortos, para no estresar más al usuario."
       },
       ...messagesHistory.map((m) => m.toJson()).toList()
-      //TODO: Tenemos que hacer una funcion que incluya en el mensaje sin que salga adentro el nivel de estress del usuario despues del test de estress y tiene que ir con 'Nivel de estress: 0.0' 
     ],
-    maxToken: 100,
-    temperature: 0.2,
+    maxToken: 3000,
   );
   
   final response = await OpenAIClient.openAI.onChatCompletion(request: request);
