55f37d421d3249d987e0ab5a330bb524bd8c4bfa
Sergio Laptop <escalantesergio821@gmail.com>
2024-04-03 17:40:55 -0400

    Vuelta al pasado, no se pudo implementar asistente del todo

--- lib/Clients/OpenAIClient.dart
+++ lib/Clients/OpenAIClient.dart
@@ -1,6 +1,5 @@
 import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
 import 'package:chatbot_psicologia/APIs/consts.dart';
-import 'package:dash_chat_2/dash_chat_2.dart';
 
 class OpenAIClient {
   static final OpenAI openAI = OpenAI.instance.build(
@@ -8,43 +7,4 @@ class OpenAIClient {
     baseOption: HttpSetup(receiveTimeout: const Duration(seconds: 5)),
     enableLog: true,
   );
-
-  // Método para crear una ejecución (Run) utilizando un asistente y un hilo específicos
-  static Future<void> createRun(String threadId) async {
-    final request = CreateRun(assistantId: 'asst_5H7O3C5O7Goh1QtoxTgDRcQa');
-    await openAI.threads.runs.createRun(threadId: threadId, request: request);
-  }
-
-  // Método para crear un hilo y ejecución con el primer mensaje
-  static Future<String> createThreadAndRun(String initialMessage) async {
-    final request = CreateThreadAndRun(assistantId: 'asst_5H7O3C5O7Goh1QtoxTgDRcQa', thread: {
-      "messages": [
-        {"role": "user", "content": initialMessage} 
-      ],
-    });
-    final response = await openAI.threads.runs.createThreadAndRun(request: request);
-    return response.threadId; 
-  }
-
-  // Método para enviar un mensaje a un hilo existente
-  static Future<void> createMessage(String threadId, String message) async {
-    final request = CreateMessage(role: 'user', content: message);
-    await openAI.threads.messages.createMessage(threadId: threadId, request: request);
-  }
-
-  static Future<List<ChatMessage>> listMessage(String threadId) async {
-
-    List<ChatMessage> messages = [];
-
-    // Aquí iría el código para llamar a la API y llenar la lista de mensajes.
-    // Por ejemplo:
-    // final response = await algunaFuncionParaObtenerMensajes(threadId);
-    // response.forEach((msgData) {
-    //   messages.add(ChatMessage(desde msgData));
-    // });
-
-    return messages;
-  }
-
-  // Otros métodos (listMessage, retrieveMessage, etc.) pueden ser añadidos aquí
 }
