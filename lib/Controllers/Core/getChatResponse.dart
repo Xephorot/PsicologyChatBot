9a5308c0a8d8e28da68a0a2bc589c89f9a8b0832
Sergio Laptop <escalantesergio821@gmail.com>
2024-04-18 17:39:27 -0400

    Tareas establecidas y arreglado instruccion

--- lib/Controllers/Core/getChatResponse.dart
+++ lib/Controllers/Core/getChatResponse.dart
@@ -23,19 +23,26 @@ Future<void> getChatResponse(ChatMessage message,
       {
         "role": "system",
         "content":
-            "Actúa como Freud, ofreciendo orientación sobre el estrés. Inicia con un test de estrés que se basa en el test de estrés de de Autoestima de Rosenberg cada mensaje que des debe ser menos de 10 palabras, solo debes dar una pregunta por cada respuesta. Si se niegan, tienes que decir al final de la conversación (Deteniendo Conversación); si no, evalúa su nivel de estrés, también debes mostrar este en cada respuesta tuya el nivel de estrés de la siguiente manera: (Nivel de estrés: Nivel). Da consejos generales, y si el estrés es alto, recomienda ayuda profesional."
+            "Actúa como Freud, ofreciendo orientación sobre el estrés. Inicia con un test de estrés que se basa en el test de estrés de de Autoestima de Rosenberg cada mensaje que des debe ser menos de 10 palabras, solo debes dar una pregunta por cada respuesta. Si se niegan, tienes que decir al final de la conversación (Deteniendo Conversación); si no, evalúa su nivel de estrés, también debes mostrar este en cada respuesta tuya el nivel de estrés de la siguiente manera: (Nivel de estrés: Nivel). Da consejos generales, y si el estrés es alto, recomienda ayuda profesional. Recuerda todoas las respuestas que des debe ser cortas y no pasar de las 10 palabras."
       },
       ...messagesHistory.map((m) => m.toJson())
     ],
     temperature: 0.1,
   );
 
+  //! Tenemos que implementar un sistema de try except para manejar errores.
+  //! Bloquear Chat por 3 segundos por cada mensaje mandado por el usuario
+  //! Agregar limite de caracteres para el usuario
+  //! Mensajes de esperar, reintentar cuando halla un error
+  
   final response = await OpenAIClient.openAI.onChatCompletion(request: request);
 
   //TODO: Implementar Prints o console logs, que delimite que es que print
   if (response != null && response.choices.isNotEmpty) {
     for (var element in response.choices) {
       if (element.message != null) {
+
+        //TODO: En lugar de imprimir en consola, que guarde todo el historial de mensajes en una basa de datos MySQL
         //print('ChatGPT: ${element.message!.content}');
 
         ChatMessageModel.messages.insert(
