625fff8b44dc4d5d5266e6878e0a4ece6c644a50
Sergio Laptop <escalantesergio821@gmail.com>
2024-04-25 16:59:57 -0400

    Agregado .env, eliminar mas tarde

--- lib/Clients/OpenAIClient.dart
+++ lib/Clients/OpenAIClient.dart
@@ -1,9 +1,9 @@
 import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
-import 'package:chatbot_psicologia/APIs/consts.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class OpenAIClient {
   static final OpenAI openAI = OpenAI.instance.build(
-    token: OPENAI_API_KEY,
+    token: dotenv.env['OPENAI_API_KEY']!, 
     baseOption: HttpSetup(receiveTimeout: const Duration(seconds: 5)),
     enableLog: true,
   );
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:chatbot_psicologia/Views/ChatPage.dart';
 import 'package:flutter/material.dart';
-
-void main() {
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const MainApp());
 }
 
