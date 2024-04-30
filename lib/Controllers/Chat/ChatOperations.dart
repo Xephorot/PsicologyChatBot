eccf22bf0727549cdd1a6a6d37a6ddb8ace0cf99
Jsantelo21 <118323306+Jsantelo21@users.noreply.github.com>
2024-04-30 09:27:58 -0400

    Add files via upload

---
+++ lib/Controllers/Chat/ChatOperations.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:chatbot_psicologia/Models/ChatUserModel.dart';
+class ChatOperations {
+  Future<void> restartAndSaveChat(BuildContext context, VoidCallback onChatRestarted) async {
+    final String chatJson = _saveChatToJSON();
+    final directory = await getApplicationDocumentsDirectory();
+    final file = File('${directory.path}/saved_chat.json');
+    await file.writeAsString(chatJson);
+    _restartChat();
+    onChatRestarted();
+  }
+  String _saveChatToJSON() {
+    List<Map<String, dynamic>> messagesJson = ChatMessageModel.messages.map((message) => {
+      'user': message.user.id,
+      'createdAt': message.createdAt.toIso8601String(),
+      'text': message.text,
+    }).toList();
+    return jsonEncode(messagesJson);
+  }
+  void _restartChat() {
+    ChatMessageModel.messages.clear();
+    ChatMessageModel.typingUsers.clear();
+  }
+}
