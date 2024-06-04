52d3c0431b5ecec76a60cd60da166a07a61cb131
Sergio Laptop <escalantesergio821@gmail.com>
2024-06-04 17:01:03 -0400

    Ordenes actualizadas

--- lib/Clients/OpenAIClient.dart
+++ lib/Clients/OpenAIClient.dart
@@ -2,6 +2,7 @@ import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class OpenAIClient {
+  //TODO: Agregar boton para cambiar el token
   static final OpenAI openAI = OpenAI.instance.build(
     token: dotenv.env['OPENAI_API_KEY']!, 
     baseOption: HttpSetup(receiveTimeout: const Duration(seconds: 5)),
