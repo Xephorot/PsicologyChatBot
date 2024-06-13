52a126bd780a8290d784c41e328ef38422efa706
Sergio Laptop <escalantesergio821@gmail.com>
2024-06-13 14:54:59 -0400

    Agregaco logo en el perfil

--- lib/Controllers/MenuLateral/UserProfile.dart
+++ lib/Controllers/MenuLateral/UserProfile.dart
@@ -1,7 +1,9 @@
 import 'package:chatbot_psicologia/Controllers/Chat/ProfileEdition.dart';
 import 'package:flutter/material.dart';
+
 class UserProfile extends StatelessWidget {
   const UserProfile({super.key});
+  
   @override
   Widget build(BuildContext context) {
     return ValueListenableBuilder(
@@ -12,11 +14,18 @@ class UserProfile extends StatelessWidget {
           child: UserAccountsDrawerHeader(
             accountName: Text(profile.username),
             accountEmail: const Text(""),
-            currentAccountPicture: const CircleAvatar(
-              child: FlutterLogo(size: 45.0),
+            currentAccountPicture: CircleAvatar(
+              child: ClipOval(
+                child: Image.asset(
+                  'android/assets/images/psicoapp logo.jpg',
+                  fit: BoxFit.cover,
+                  width: 90.0,
+                  height: 90.0,
+                ),
+              ),
             ),
             decoration: const BoxDecoration(
-                color: Colors.green,
+              color: Colors.green,
             ),
           ),
         );
